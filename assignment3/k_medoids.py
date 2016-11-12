e31e1e0dc673e5045bb8de26ce1bea7f17b6a817
Zhang Han <463808243@qq.com>
2016-11-12 16:01:46 +0800

    last commit

--- assignment3/k_medoids.py
+++ assignment3/k_medoids.py
@@ -54,6 +54,13 @@ def get_dist(data_mat):
     
     return dist_dict
     
+def point_dist(data_mat,i,j):
+    
+    diff = data_mat[i,:] - data_mat[j,:]
+    diff = abs(diff)
+    dist = sum(diff)
+    return dist
+    
     
 def get_cost(k_index, dist_dict, feature_mat,labels ):
     
@@ -73,12 +80,39 @@ def get_cost(k_index, dist_dict, feature_mat,labels ):
         
         #累加距离， cost
         cost = cost + min_dist
+    
+    return cost, cluster_labels
+    
+def get_cost_bigdata(k_index, feature_mat,labels ):
+    
+    data_size = feature_mat.shape[0]
+    cluster_labels = np.zeros(data_size)
+    cost = 0
+
+        
+    #将每个数据点 i 分配到最近的中心点
+    for i in range(feature_mat.shape[0]):
+        medoids_mat = feature_mat[k_index, : ]
+        point = feature_mat[i,:]
+        k_size = medoids_mat.shape[0]
+        diff_mat = np.tile(point, (k_size,1)) - medoids_mat
+    
+        diff_mat = abs(diff_mat)    
+        dist = diff_mat.sum(axis=1)
+        
+        #排序， 取距离最近的中心点
+        sort_dist_index = dist.argsort()
+        nearest_medoid_index = k_index[sort_dist_index[0]]
+        dist = point_dist(feature_mat,i,nearest_medoid_index)
+        cluster_labels[i] = labels[nearest_medoid_index]
+        cost = cost + dist
+        
         
     return cost, cluster_labels
 
 
-# 找到一个代价最小的点替换第i个中心点                 
-def replace_i(k_index, k, dist_dict, feature_mat,labels):
+# 找到一个代价最小的点替换第k个中心点                 
+def replace_k(k_index, k, dist_dict, feature_mat,labels):
     #是否替换的标志
     is_replaced = False
     min_cost, cluster_labels = get_cost(k_index, dist_dict, feature_mat,labels)
@@ -91,6 +125,34 @@ def replace_i(k_index, k, dist_dict, feature_mat,labels):
         new_kindex = np.array(k_index)
         new_kindex[k] = i
         new_cost, new_labels = get_cost(new_kindex, dist_dict, feature_mat,labels)
+
+        if min_cost > new_cost:
+            is_replaced = True
+            min_cost = new_cost
+            min_index = new_kindex
+            min_labels = new_labels
+    
+    return is_replaced , min_index, min_cost, min_labels
+    
+# 找到一个代价最小的点替换第k个中心点                 
+def replace_k_bigdata(k_index, k,  feature_mat,labels):
+    #是否替换的标志
+    is_replaced = False
+    min_cost, cluster_labels = get_cost_bigdata(k_index, feature_mat,labels)
+    min_index = np.array(k_index)
+    min_labels = labels
+    
+    # 随机选出sample_num个点作为候选点， 来替换第 K 个中心点， 再计算距离
+    sample_num = 3
+    candidate = k_center(feature_mat.shape[0],sample_num)
+    
+    for i in candidate:
+        if i in k_index:
+            continue
+        new_kindex = np.array(k_index)
+        new_kindex[k] = i
+        #new_cost, new_labels = get_cost(new_kindex, dist_dict, feature_mat,labels)
+        new_cost, new_labels = get_cost_bigdata(new_kindex, feature_mat,labels)
 #        print(new_kindex)
 #        print(new_cost)
         if min_cost > new_cost:
@@ -106,10 +168,12 @@ def k_medoids(feature_mat, labels,dist_dict, k):
     #随机选择K个点作为初始medoid
     data_size = feature_mat.shape[0]
     k_index =  k_center(data_size,k) 
-    print(k_index)
+   
     init_cost, init_labels = get_cost(k_index, dist_dict, feature_mat,labels)
-    print(init_cost)
-    print('----------')
+    #print('----initial------')
+    #print(k_index)
+    #print(init_cost)
+
     is_replaced = True
     min_cost = init_cost
     min_labels = init_labels
@@ -117,22 +181,52 @@ def k_medoids(feature_mat, labels,dist_dict, k):
     while(is_replaced):
         is_replaced = False
         for i in range(k):
-            print('-------------in: ', i,'------------')
-            is_replaced, new_index, new_cost, new_labels = replace_i(k_index, i, dist_dict, feature_mat,labels)
+            #print('-------------in: ', i,'------------')
+            is_replaced, new_index, new_cost, new_labels = replace_k(k_index, i, dist_dict, feature_mat,labels)
             if is_replaced == True:
                 #更新中心点 和 最小cost
                 k_index = new_index
                 min_cost = new_cost
                 min_labels = new_labels
-                print(k_index, min_cost)
+                #print(k_index, min_cost)
+                #print(min_labels)
                 
-    print('min index: ', k_index)
-    print('min cost:', min_cost)
+                
+    #print('k-medoids index: ', k_index)
+    #print('min cost:', min_cost)
         
     return min_labels
     
+def k_medoids_2(feature_mat, labels, k):
+    #随机选择K个点作为初始medoid
+    data_size = feature_mat.shape[0]
+    k_index =  k_center(data_size,k) 
+   
+    init_cost, init_labels = get_cost_bigdata(k_index,  feature_mat,labels)
     
-def get_purity(true_labels, test_labels,k ):
+    min_cost = init_cost
+    min_labels = init_labels
+    
+    iterate_times = 3
+    for time in range(iterate_times):
+        for i in range(k):
+            is_replaced, new_index, new_cost, new_labels = replace_k_bigdata(k_index, i, feature_mat,labels)
+            if is_replaced == True:
+                #更新中心点 和 最小cost
+                k_index = new_index
+                min_cost = new_cost
+                min_labels = new_labels
+                
+                
+                
+#    print('k-medoids index: ', k_index)
+#    print('min cost:', min_cost)   
+    return min_labels
+    
+# get purity and gini index
+def get_result(true_labels, test_labels,k ):
+#    print(true_labels)
+#    print(test_labels)
     n = len(true_labels)
     confusion_mat = np.zeros([k,k],int)
     for i in range(n):
@@ -147,7 +241,7 @@ def get_purity(true_labels, test_labels,k ):
     sum_p = np.sum(p)
     sum_m = np.sum(m)
     purity = sum_p/sum_m
-    print(sum_p,sum_m,purity)
+    print('purity:',purity)
 
     # gini index
     g = np.zeros(k)
@@ -160,42 +254,50 @@ def get_purity(true_labels, test_labels,k ):
     for i in range(k):
         sum_gm = sum_gm + g[i] * m[i]
     gini_index = sum_gm / sum_m
-    print(gini_index)
+    print('gini_index:',gini_index)
         
-    
-
-if __name__ == '__main__':  
-    feature_mat, labels = load_data('german.txt')
+#代价最小替换中心点，遍历所有的非中心点，复杂度高    
+def k_medoids_smalldata(feature_mat, labels):
+    k = 2
     dist_dict = get_dist(feature_mat)
-    k = 2    
+        
     min_labels = k_medoids(feature_mat, labels,dist_dict, k)
-    
-    result = min_labels - labels
-    result = result==0
-    result = sum(result)/len(result)
-    print(result)
-   
-    
-    print('***********')
-    
-    #把-1元素变为0
+     
     true_labels = np.array(labels==1,dtype=int)
     test_labels = np.array(min_labels==1,dtype=int)
-    get_purity(true_labels, test_labels, k )
-#    l=np.array([-1,  1, -1, -1,  1, -1, -1])
-#    temp = np.array(l==1,dtype=int)
-#    print(l)
-#    print(temp)
-  
-   
-#    
-#    print(get_cost(np.array([7,2]), dist_dict, feature_mat,labels ))
-#    
-#    
-    '''
-    min index:  [7 2]
-min cost: 15.0
-0.454545454545
-[ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]
-    '''
+    
+    
+    get_result(true_labels, test_labels, k )
+    
+    
+# 随机选择替换中心点，复杂度低    
+def k_medoids_smalldata_2(feature_mat, labels):
+    k = 2   
+    min_labels = k_medoids_2(feature_mat, labels, k)  
+    true_labels = np.array(labels==1,dtype=int)
+    test_labels = np.array(min_labels==1,dtype=int)
+    get_result(true_labels, test_labels, k )
+    
+# 随机选择替换中心点，复杂度低 
+def k_medoids_bigdata(feature_mat, labels):
+    
+    k = 10 
+    true_labels = np.array(k_medoids_2(feature_mat, labels, k), dtype = int)
+    test_labels = np.array(labels, dtype = int)
+    
+    
+    get_result(true_labels, test_labels, k )
+    
+
+
+    
+if __name__ == '__main__': 
+    print('-----------dataset1 german.txt:')
+    feature_mat, labels = load_data('german.txt')
+    k_medoids_smalldata(feature_mat, labels)
+    
+    print('-----------dataset2 mnist.txt')
+    feature_mat, labels = load_data('mnist.txt')
+    k_medoids_bigdata(feature_mat, labels)
+    
     
--- assignment3/spectral_clustering.py
+++ assignment3/spectral_clustering.py
@@ -4,4 +4,128 @@ Created on Sun Oct 30 13:35:36 2016
 
 @author: han
 """
+import numpy as np
+from k_medoids import k_medoids_smalldata 
+from k_medoids import k_medoids_bigdata
 
+
+def load_data(file_name, delim = ','):
+    fopen = open(file_name)
+    temp_lines = [line.strip().split(delim) for line in fopen.readlines()]
+    data_mat = np.array(temp_lines, np.float)
+    feature_mat = data_mat[:,:-1]
+    labels = data_mat[:,-1]
+    
+    return feature_mat, labels
+
+#计算给定点inx 的最近n个点的距离 
+def knn_dist(inx, data_mat, n):
+    dataset_size = data_mat.shape[0]
+    diff_mat = np.tile(inx, (dataset_size,1)) - data_mat
+      
+    diff_mat = np.asarray(diff_mat)
+    sq_diffmat = diff_mat**2
+    sq_dist = sq_diffmat.sum(axis=1)
+    dist = sq_dist**0.5
+          
+    #距离最小的k个向量,除去第一个自己，
+#    sort_dist_index = dist.argsort()  
+#    n_neighbor = []
+#    for i in range(1,n+1):
+#        #n_neighbor[i-1] = int(sort_dist_index[i])
+#        n_neighbor.append(sort_dist_index[i])
+#            
+#    print(n_neighbor)
+    
+    index = get_min_index(dist, n+1)
+    n_neighbor = index[1:]
+   
+        
+    return n_neighbor
+    
+def con_graph(data_mat, n):
+    size = data_mat.shape[0]
+    graph = np.zeros([size,size],int)
+    for i in range(size):
+        n_neighbor = knn_dist(data_mat[i,:], data_mat, n)
+        #print('in con graph', i)
+        for j in n_neighbor:
+            graph[i][j] = 1
+            graph[j][i] = 1
+        
+    return graph
+    
+    
+def spectral_clustering(feature_mat, n, k): 
+    
+    graph = con_graph(feature_mat, n)   
+    
+    # D_mat is diagonal weight matrix    
+    col_sum = graph.sum(axis=0) 
+    #D_mat = np.diag(col_sum)
+    size = feature_mat.shape[0]
+    D_mat = np.zeros([size,size],int)
+    for i in range(size):
+        D_mat[i][i] = col_sum[i]
+    
+    #print('D', D_mat)  
+    D_mat = np.mat(D_mat)
+    graph = np.mat(graph)
+    
+    L_mat = D_mat - graph
+    X_mat = np.dot(np.linalg.inv(D_mat) , L_mat)
+    eig_vals, eig_vects = np.linalg.eig(X_mat)
+    sorted_index = np.argsort(eig_vals)
+    #print(eig_vals[sorted_index][0:10])
+    k_index = sorted_index[1:k+1]
+    k_vects = eig_vects[: , k_index]
+    
+    return k_vects
+
+
+
+def spec_smalldata(feature_mat,labels, n):
+    k = 2
+    Y = spectral_clustering(feature_mat, n, k) 
+    k_medoids_smalldata(Y, labels)
+    
+def spec_bigdata(feature_mat,labels, n):
+    k = 10 
+    Y = spectral_clustering(feature_mat, n, k)
+    k_medoids_bigdata(Y, labels)
+    
+     
+        
+def get_min_index(a, n):
+    l = a.tolist()
+    max_num = max(l)+1
+    index = []
+    for i in range(n):
+        min_val = min(l)
+        min_index = l.index(min_val)
+        index.append(min_index)
+        l[min_index] = max_num
+        
+    return index
+        
+    
+if __name__ == '__main__':  
+    print('-----------dataset1 german.txt:')
+    feature_mat, labels = load_data('german.txt')
+    for n in [3,6,9]:
+        print('n= ',n,':')
+        spec_smalldata(feature_mat, labels, n)
+    
+   
+    print('-----------dataset2 mnist.txt:')
+    feature_mat, labels = load_data('mnist.txt')
+    for n in [3,6,9]:
+        print('n= ',n,':')
+        spec_bigdata(feature_mat, labels, n)
+    
+   
+    
+
+    
+    
+    
